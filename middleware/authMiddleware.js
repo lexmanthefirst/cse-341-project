f7d46ce7b8293f0522ea935a71d16d82ed1f9831
Lexmanthefirst <lexmanthefirst@gmail.com>
2025-06-16 08:19:16 +0100

    fix(auth): fixed auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,22 @@
-exports.isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) return next();
-  res.status(401).json({ message: 'Not authenticated' });
+const isAuthenticated = (req, res, next) => {
+  if (req.session.user === undefined) {
+    res.status(401).json({ message: 'Not authenticated' });
+  }
+  next();
 };
 
-exports.authorizeRoles = (...roles) => {
+const authorizeRoles = (...roles) => {
   return (req, res, next) => {
     if (!req.user || !roles.includes(req.user.role)) {
       return res.status(403).json({ message: 'Forbidden' });
     }
     next();
   };
+};
+
+module.exports = {
+  isAuthenticated,
+  authorizeRoles,
 };
 // exports.isAdmin = (req, res, next) => {
 //   if (req.user && req.user.role === 'admin') return next();
