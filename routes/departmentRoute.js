15770f39426b962b1c6a6c9698d8dc984912aa32
Lexmanthefirst <lexmanthefirst@gmail.com>
2025-06-16 13:27:10 +0100

    fix:  imported file path

--- routes/departmentRoute.js
+++ routes/departmentRoute.js
@@ -4,7 +4,7 @@ const departmentController = require('../controllers/departmentController');
 const departmentValidator = require('../utilities/departmentValidator');
 const Util = require('../utilities');
 const authenticateUser = require('../middleware/authMiddleware');
-const authorizeRoles = require('../middleware/authorizeRolesMiddleware');
+const authorizeRoles = require('../middleware/authoRole');
 
 /**
  * @swagger
